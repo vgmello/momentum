7cad39c00a2b5dc40958a9d120c27f932a51a994
Vitor M <4777793+vgmello@users.noreply.github.com>
2025-08-18 04:52:46 +0100

    Refactor workflows and clean up unnecessary comments comments inability. Enabled sticky comments code review refinements structure.

--- src/AppDomain.Api/GlobalUsings.cs
+++ src/AppDomain.Api/GlobalUsings.cs
@@ -2,7 +2,9 @@
 
 global using AppDomain;
 global using AppDomain.Api;
+//#if (INCLUDE_SAMPLE)
 global using AppDomain.Api.Cashiers.Mappers;
+//#endif
 global using Grpc.Core;
 global using Microsoft.AspNetCore.Mvc;
 global using Riok.Mapperly.Abstractions;
