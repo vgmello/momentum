13f84506cef3537e05a08159ddf3c0df3000c930
Vitor M <4777793+vgmello@users.noreply.github.com>
2025-08-20 03:48:10 +0100

    Add lazy clients `LazyManager` to manage delayed client start and ensure proper service.

--- src/AppDomain.Api/Infrastructure/Extensions/OrleansExtensions.cs
+++ src/AppDomain.Api/Infrastructure/Extensions/OrleansExtensions.cs
@@ -25,6 +25,8 @@ public static class OrleansExtensions
             }
         });
 
+        SetupLazyClusterClients(builder);
+
         builder.Services
             .AddOpenTelemetry()
             .WithMetrics(opt => opt.AddMeter("Microsoft.Orleans"))
@@ -35,5 +37,119 @@ public static class OrleansExtensions
             });
 
         return builder;
+    }
+
+    private static void SetupLazyClusterClients(IHostApplicationBuilder builder)
+    {
+        var clusterClientType = typeof(IClusterClient);
+
+        var hostedClusterClientRegistrations = builder.Services
+            .Where(r => r.ServiceType == typeof(IHostedService) && IsClusterClientService(r, clusterClientType) && !r.IsKeyedService)
+            .ToList();
+
+        foreach (var reg in hostedClusterClientRegistrations)
+        {
+            builder.Services.Remove(reg);
+        }
+
+        var primaryClusterClientRegistration = builder.Services.Last(r => r.ServiceType == clusterClientType && !r.IsKeyedService);
+        var primaryKeyedClusterClient = CreateKeyedClusterClient(primaryClusterClientRegistration);
+
+        var factory = LazyClusterClientFactory(primaryClusterClientRegistration);
+        var newPrimaryClusterClient = new ServiceDescriptor(clusterClientType, factory, primaryKeyedClusterClient.Lifetime);
+
+        builder.Services.Add(newPrimaryClusterClient);
+        builder.Services.AddHostedService<LazyClusterClientManager>();
+    }
+
+    private static Func<IServiceProvider, object> LazyClusterClientFactory(ServiceDescriptor primaryClusterClientRegistration)
+    {
+        var initialized = false;
+        var initializing = false;
+
+        return provider =>
+        {
+            var primaryClusterClient = provider.GetRequiredKeyedService<IClusterClient>(primaryClusterClientRegistration);
+
+            if (initialized)
+                return primaryClusterClient;
+
+            var lazyClusterClient = provider.GetRequiredService<LazyClusterClientManager>();
+
+            if (initializing)
+            {
+                // This should the main ClusterClient initialization (first call)
+                lazyClusterClient.StartDelayedAsync(primaryClusterClient);
+
+                return primaryClusterClient;
+            }
+
+            initializing = true;
+
+            // This call should trigger this same factory again, which it will be in the `initializing` state, returning the main client
+            var clients = provider.GetServices<IClusterClient>().ToList();
+            clients.ForEach(lazyClusterClient.StartDelayedAsync);
+
+            initialized = true;
+
+            return primaryClusterClient;
+        };
+    }
+
+    private static ServiceDescriptor CreateKeyedClusterClient(ServiceDescriptor registration)
+    {
+        var serviceKey = registration;
+
+        if (registration.ImplementationInstance is not null)
+            return new ServiceDescriptor(typeof(IClusterClient), serviceKey, registration.ImplementationInstance!);
+
+        if (registration.ImplementationFactory is not null)
+        {
+            return new ServiceDescriptor(typeof(IClusterClient), serviceKey, static (provider, key) =>
+                ((ServiceDescriptor)key!).ImplementationFactory!.Invoke(provider), registration.Lifetime);
+        }
+
+        return new ServiceDescriptor(typeof(IClusterClient), serviceKey, registration.ImplementationType!);
+    }
+
+    private static bool IsClusterClientService(ServiceDescriptor serviceDescriptor, Type? clusterClientType)
+    {
+        static bool IsClusterClientFactory(Func<IServiceProvider, object?>? factory)
+        {
+            // I couldn't find a better way to check if the factory is a cluster client factory
+            var implementationField = factory?.Target?.GetType().GetField("implementation");
+
+            if (implementationField is null)
+                return false;
+
+            return (implementationField.GetValue(factory!.Target) as Type)?.IsAssignableTo(typeof(IClusterClient)) == true;
+        }
+
+        return IsClusterClientFactory(serviceDescriptor.ImplementationFactory) ||
+               serviceDescriptor.ImplementationType?.IsAssignableTo(clusterClientType) == true;
+    }
+
+    private sealed class LazyClusterClientManager : IHostedService
+    {
+        private readonly HashSet<IHostedService> _startedClients = [];
+
+        public void StartDelayedAsync(IClusterClient clusterClient)
+        {
+            if (clusterClient is not IHostedService hostedClient || _startedClients.Contains(hostedClient))
+                return;
+
+            hostedClient.StartAsync(CancellationToken.None).GetAwaiter().GetResult();
+
+            _startedClients.Add(hostedClient);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            var stopTasks = _startedClients.Select(c => c.StopAsync(cancellationToken));
+
+            return Task.WhenAll(stopTasks);
+        }
     }
 }
