8d76d33b0b3b33ee24d745b8f57e8034784675d7
Vitor M <4777793+vgmello@users.noreply.github.com>
2026-02-22 20:50:50 +0000

    Add global using directive for System.Diagnostics.CodeAnalysis

--- src/AppDomain.Contracts/GlobalUsings.cs
+++ src/AppDomain.Contracts/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Copyright (c) OrgName. All rights reserved.
 
+global using System.Diagnostics.CodeAnalysis;
 //#if (INCLUDE_SAMPLE)
 global using Momentum.Extensions.Abstractions.Messaging;
 //#endif
